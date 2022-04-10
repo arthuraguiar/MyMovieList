b77392ec9e8322b93245d69064217b59499cf123
arthur aguiar <arthur.aguiar@picpay.com>
2022-04-10 17:01:51 -0300

    rearranjando artefatos de teste

--- app/src/test/java/com/example/mymovieslist/data/datasource/MoviesDataSourceTest.kt
+++ app/src/test/java/com/example/mymovieslist/data/datasource/MoviesDataSourceTest.kt
@@ -1,13 +1,14 @@
-package com.example.mymovieslist.data
+package com.example.mymovieslist.data.datasource
 
 import app.cash.turbine.test
-import com.example.mymovieslist.data.datasource.MoviesDataSource
-import com.example.mymovieslist.data.datasource.MoviesDataSourceImpl
 import com.example.mymovieslist.data.network.MovieService
 import com.example.mymovieslist.domain.model.RequestExceptions
+import com.example.mymovieslist.stubs.fetchPopularMoviesResponse
+import com.example.mymovieslist.stubs.httpException
+import com.example.mymovieslist.stubs.socketTimeoutException
 import io.mockk.coEvery
 import io.mockk.mockk
-import junit.framework.Assert.assertEquals
+import org.junit.Assert.assertEquals
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import org.junit.Before
 import org.junit.Test
@@ -33,6 +34,26 @@ class MoviesDataSourceTest {
             // Given
             val exception = socketTimeoutException
             val expected = RequestExceptions.NoConnectionException
+            coEvery {
+                service.fetchPopularMovies()
+            } throws exception
+
+            // When
+            val result = moviesDataSource.fetchPopularMovies()
+
+            // Then
+            result.test {
+                val error = expectError()
+                assertEquals(error, expected)
+            }
+        }
+
+    @Test
+    fun `fetchPopularMovie should throw HttpError when service receives HttpException`() =
+        runTest {
+            // Given
+            val exception = httpException
+            val expected = RequestExceptions.HttpError(exception.message(), exception.code())
             coEvery {
                 service.fetchPopularMovies()
             } throws exception
--- app/src/test/java/com/example/mymovieslist/stubs/DataStubs.kt
+++ app/src/test/java/com/example/mymovieslist/stubs/DataStubs.kt
@@ -1,10 +1,13 @@
-package com.example.mymovieslist.data
+package com.example.mymovieslist.stubs
 
 import com.example.mymovieslist.data.model.FetchPopularMoviesResponse
 import com.example.mymovieslist.data.model.MovieResponse
+import io.mockk.mockk
+import retrofit2.HttpException
 import java.net.SocketTimeoutException
 
 val socketTimeoutException = SocketTimeoutException()
+val httpException: HttpException = mockk(relaxed = true)
 
 val fetchPopularMoviesResponse = FetchPopularMoviesResponse(
     results = listOf(
