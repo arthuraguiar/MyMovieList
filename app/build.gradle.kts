dab3c2b48ad1c74171d70d35d6ae3b113eb2d6bf
Luiz Filipe Medeira <Luiz.fmdr@gmail.com>
2023-02-16 21:02:37 -0300

    update_dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,4 +1,3 @@
-
 import java.io.FileInputStream
 import java.io.FileNotFoundException
 import java.util.Properties
@@ -63,54 +62,48 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.8.0")
-    implementation("androidx.appcompat:appcompat:1.4.2")
-    implementation("com.google.android.material:material:1.5.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.1")
-    implementation("androidx.activity:activity-ktx:1.5.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.5.0")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.5.0")
-    implementation("androidx.lifecycle:lifecycle-process:2.5.0")
+    implementation(platform("androidx.compose:compose-bom:2022.12.00"))
+    implementation("androidx.compose.foundation:foundation")
+    implementation("androidx.compose.material:material")
+    implementation("androidx.compose.material3:material3")
+    implementation("androidx.compose.animation:animation")
+    implementation("androidx.compose.material:material-icons-core")
+    implementation("androidx.compose.material:material-icons-extended")
+    implementation("androidx.compose.material3:material3-window-size-class")
+    implementation("androidx.compose.ui:ui")
+    implementation("androidx.compose.ui:ui-tooling-preview")
+    implementation("androidx.compose.ui:ui-tooling")
+
+    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4")
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.okhttp3:logging-interceptor:4.10.0")
     implementation("com.jakewharton.retrofit:retrofit2-kotlinx-serialization-converter:0.8.0")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.2")
     implementation("androidx.navigation:navigation-compose:2.5.3")
+    implementation("androidx.hilt:hilt-navigation-compose:1.0.0")
+    implementation("androidx.activity:activity-compose:1.6.1")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.5.1")
 
-    implementation("com.google.dagger:hilt-android:2.38.1")
+    implementation("com.google.dagger:hilt-android:2.44.2")
+    kapt("com.google.dagger:hilt-android-compiler:2.43.2")
+
     implementation(project(mapOf("path" to ":theme")))
-    kapt("com.google.dagger:hilt-android-compiler:2.38.1")
-
-
-    implementation("androidx.compose.material:material-icons-extended:1.3.0-alpha01")
-    implementation("androidx.compose.material3:material3-window-size-class:1.0.0-alpha14")
 
     implementation("com.github.skydoves:landscapist-coil:1.5.0")
-    implementation("com.google.accompanist:accompanist-swiperefresh:0.24.13-rc")
-    implementation("com.google.accompanist:accompanist-systemuicontroller:0.24.13-rc")
-    implementation("com.google.android.material:compose-theme-adapter:1.1.1")
-    // Integration with activities
-    implementation("androidx.activity:activity-compose:1.5.0")
-    // Compose Material Design
-    implementation("androidx.compose.material:material:1.1.1")
-    // Animations
-    implementation("androidx.compose.animation:animation:1.1.1")
-    // Tooling support (Previews, etc.)
-    implementation("androidx.compose.ui:ui-tooling:1.1.1")
-    // Integration with ViewModels
-    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.5.0")
-    // UI Tests
+    implementation("com.google.accompanist:accompanist-swiperefresh:0.28.0")
+    implementation("com.google.accompanist:accompanist-systemuicontroller:0.28.0")
 
     testImplementation("io.mockk:mockk:1.12.2")
-    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.6.1")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.6.4")
     testImplementation("com.google.truth:truth:1.1.3")
     testImplementation("androidx.arch.core:core-testing:2.1.0")
-    testImplementation("app.cash.turbine:turbine:0.5.2")
+    testImplementation("app.cash.turbine:turbine:0.12.1")
     testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.1.1")
-    androidTestImplementation("androidx.test.ext:junit:1.1.3")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
 
 kapt {
--- theme/build.gradle.kts
+++ theme/build.gradle.kts
@@ -39,7 +39,7 @@ dependencies {
     val composeBom = platform("androidx.compose:compose-bom:2023.01.00")
     implementation(composeBom)
 
-    implementation("androidx.appcompat:appcompat:1.4.2")
+    implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.6.0")
     implementation("androidx.compose.material3:material3:1.0.1")
     implementation("androidx.compose.material:material")
