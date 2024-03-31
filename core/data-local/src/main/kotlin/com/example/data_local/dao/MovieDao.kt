802f5f4fcb3a336b29f7303322b5d249d4353aeb
arthuaguiar <barcellosdeaguiar@hotmail.com>
2024-03-31 17:51:40 -0300

    adding schema and dao configs

--- core/data-local/src/main/kotlin/com/example/data_local/dao/MovieDao.kt
+++ core/data-local/src/main/kotlin/com/example/data_local/dao/MovieDao.kt
@@ -1,15 +1,17 @@
 package com.example.data_local.dao
 
+import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.example.data_local.entities.MovieEntity
 
+@Dao
 interface MovieDao {
 
     @Query("SELECT * FROM movie_entity where page = :page")
     suspend fun getMoviesFromPage(page: Int): List<MovieEntity>
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun insertAll(vararg movie: List<MovieEntity>)
+    suspend fun insertAll(vararg movie: MovieEntity)
 }
