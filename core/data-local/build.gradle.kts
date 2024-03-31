fde93cc395252881423d849ce433e544ebf57ae8
arthuaguiar <barcellosdeaguiar@hotmail.com>
2024-03-31 17:16:29 -0300

    local db

---
+++ core/data-local/build.gradle.kts
@@ -0,0 +1,17 @@
+plugins {
+    id("config.android.library")
+    id("org.jetbrains.kotlin.android")
+    id("config.android.hilt")
+    alias(libs.plugins.ksp)
+}
+
+android {
+    namespace = "com.example.data_local"
+
+}
+
+dependencies {
+
+    implementation(libs.bundles.room)
+    ksp(libs.room.runtime)
+}
---
+++ core/data-local/src/main/kotlin/com/example/data_local/entities/MovieEntity.kt
@@ -0,0 +1,21 @@
+package com.example.data_local.entities
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class MovieEntity(
+    @PrimaryKey
+    @ColumnInfo("title")
+    val title: String,
+
+    @ColumnInfo("original_language")
+    val originalLanguage: String,
+
+    @ColumnInfo("release_date")
+    val releaseDate: String,
+
+    @ColumnInfo("poster_url")
+    val posterUrl: String
+)
